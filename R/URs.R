f52460b49d09e477db9d65b4e2cea0c50deb4a1d
CWWhitney <whitney.cory@gmail.com>
2018-12-30 12:15:56 +0100

    SOrted list for URs result

--- R/URs.R
+++ R/URs.R
@@ -23,7 +23,7 @@ URs <- function(data) {
    URdata$URps <- dplyr::select(URdata, -informant, -sp_name) %>% rowSums()
     data_URs <- plyr::ddply(URdata, ~sp_name,
                 plyr::summarise, URs = sum(URps))
-    
+    URs <- sort(data_URs, decreasing = TRUE)
     print("Total number of Use Reports (URs) for each species in the data set")
-    print(data_URs)
+    print(URs)
 }
