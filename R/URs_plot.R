e0596dc62e78b0aa18f83cc9e83267cb1f8b568d
CWWhitney <whitney.cory@gmail.com>
2019-02-05 13:23:35 +0100

    URs plot (somehow lost during pull request)

---
+++ R/URs_plot.R
@@ -0,0 +1,61 @@
+#' Radial bar plot of use reports (UR) per species
+#'
+#' This function creates a radial bar plot of use reports (UR) per secies based on the `UR function`.
+#' @param data is an ethnobotany data set with column 1 'informant' and 2 'sp_name' as row identifiers of informants and of species names respectively.
+#' The rest of the columns are the identified ethnobotany use categories. The data should be populated with counts of uses per person (should be 0 or 1 values).
+#' @keywords ethnobotany, cultural value, use report
+#'
+#' @importFrom magrittr %>%
+#' @importFrom plyr ddply 
+#' @importFrom plyr summarise 
+#' @importFrom assertthat validate_that
+#' @importFrom assertthat see_if
+#' 
+#' @examples
+#' 
+#' #Use built-in ethnobotany data example
+#' URs_plot(ethnobotanydata)
+#' 
+#' #Generate random dataset of three informants uses for four species
+#' eb_data <- data.frame(replicate(10,sample(0:1,20,rep=TRUE)))
+#' names(eb_data) <- gsub(x = names(eb_data), pattern = "X", replacement = "Use_")  
+#' eb_data$informant<-sample(c('User_1', 'User_2', 'User_3'), 20, replace=TRUE)
+#' eb_data$sp_name<-sample(c('sp_1', 'sp_2', 'sp_3', 'sp_4'), 20, replace=TRUE)
+#' URs_plot(eb_data)
+#' 
+#' @export URs_plot
+URs_plot <- function(data) {
+  if (!requireNamespace("plyr", quietly = TRUE)) {
+    stop("Package \"plyr\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  
+  meltURdata <- URdata <- URs <- sp_name <- informant <- URps <- NULL # Setting the variables to NULL first, appeasing R CMD check
+  
+  #add error stops with validate_that
+  assertthat::validate_that("informant" %in% colnames(data), msg = "The required column called \"informant\" is missing from your data. Add it.")
+  assertthat::validate_that("sp_name" %in% colnames(data), msg = "The required column called \"sp_name\" is missing from your data. Add it.")
+  
+  assertthat::validate_that(is.factor(data$informant), msg = "The \"informant\" is not a factor variable. Transform it.")
+  assertthat::validate_that(is.factor(data$sp_name), msg = "The \"sp_name\" is not a factor variable. Transform it.")
+  
+  assertthat::validate_that(all(sum(dplyr::select(data, -informant, -sp_name)>0)) , msg = "The sum of all UR is not greater than zero. Perhaps not all uses have values or are not numeric.")
+  
+  ## Use 'complete.cases' from stats to get to the collection of obs without NA
+  data_complete<-data[stats::complete.cases(data), ]
+  #message about complete cases
+  assertthat::see_if(length(data_complete) == length(data), msg = "Some of your observations included \"NA\" and were removed. Consider using \"0\" instead.")
+  
+  URdata <- data #create subset-able data
+  
+  meltURdata <- dplyr::select(URdata, -informant) %>% reshape::melt() %>% plyr::ddply(~ sp_name, summarize, value = sum(value))
+  
+  URs_plot <- ggplot(meltURdata, aes(x = sp_name, y = value, fill = sp_name)) +
+    geom_bar(width = 1, stat = "identity", color = "white") +
+    scale_y_continuous(breaks = 0:nlevels(DF$variable)) +
+    coord_polar() + 
+    theme_minimal() 
+  
+  print("Radial plot of Use Reports (URs) for each species in the data set")
+  print(URs_plot)
+}
